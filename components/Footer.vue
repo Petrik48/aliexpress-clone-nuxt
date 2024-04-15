d38b60a2ba170190b1a6ef48b9e6e0b1c2551162
Petrik48 <alex.petrov.15@mail.ru>
2024-04-15 12:50:12 +0300

    22:18

---
+++ components/Footer.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div id="footerMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+    <div class="max-w-[1200px] mx-auto px-4">
+
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="ms:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">Help</div>
+          <div class="text-[#999999] text-sm">Help Center, Disputes & Reports, Buyer Protection </div>
+        </div>
+        <div class="ms:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">AliExpress Multi-Language Sites</div>
+          <div class="text-[#999999] text-sm">Russian, Portuguese, Spanish, French, German, Italian, Dutch, Turkish</div>
+        </div>
+      </div>
+
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="ms:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">Help</div>
+          <div class="text-[#999999] text-sm">Help Center, Disputes & Reports, Buyer Protection </div>
+        </div>
+        <div class="ms:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">AliExpress Multi-Language Sites</div>
+          <div class="text-[#999999] text-sm">Russian, Portuguese, Spanish, French, German, Italian, Dutch, Turkish</div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
